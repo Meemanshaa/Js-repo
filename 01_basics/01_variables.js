632adeaa5db24d81e25d90856ffb94a57cf512ef
Meemansha Sumit <139813612+Meemanshaa@users.noreply.github.com>
2026-01-01 04:08:03 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accid=213
+let accemail="meemansha@gmail.com"
+var accpass="2005"
+acccity="jbp"
+let accstate
+
+//accid=2 //not allowed 
+console.log(accid);
+accemail="hjsdsjv@gmail.com"
+accpass="7812"
+acccity="indore"
+console.table([accid,accpass,acccity,accstate])
+/* prefered not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+console.log("meemansha")
+
+let name="meemansha"
+let a=13
+let isvar=true
+let city
+
+//number => 2 to the power 53
+//biginit
+//string => ""
+//boolean => true/false
+//null => standlone value(khali), null is object
+//undefined => value not defined but var is declared
+//symbol => unique
+
+console.log(typeof name)
