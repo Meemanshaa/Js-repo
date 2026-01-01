f820e1be32e75eb593bbc51d2a6b7075a80b284a
Meemansha Sumit <139813612+Meemanshaa@users.noreply.github.com>
2026-01-01 16:58:34 +0000

    datatypes summary

---
+++ 01_basics/05_datatypes_basic.js
@@ -0,0 +1,42 @@
+//js is dynamically typed language
+//data categorization - primitive and non primitive
+//primitive data types
+// 7 types- String, Number, Boolean, null, undefined, Symbol, BigInt
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid)
+//null => type of(object)
+
+const bignumber=72815437542857n
+
+//Reference types/Non-primitve
+//types- Arrays, Objects, Functions
+//arrays
+const heros=["apple","banana","mango"];
+
+//objects
+let myobj={
+    name:"Meemansha",
+    age:20,
+}
+console.log(myobj)
+
+const myfun=function(){
+    console.log("heloo")
+}
+console.log(typeof(myfun))
+
+//Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
