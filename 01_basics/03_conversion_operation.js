75de4dabd4106b8b3cda7a362af72f2abb4104f5
Meemansha Sumit <139813612+Meemanshaa@users.noreply.github.com>
2026-01-01 04:19:33 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,24 @@
+let score="33abc"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber)
+console.log(valueinnumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+//false => 0
+
+let islogedin=1
+let booleanislogedin=Boolean(islogedin)
+console.log(booleanislogedin)
+
+//1=> true
+//0=> false
+//"hitesh" => true
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(typeof stringnumber)
