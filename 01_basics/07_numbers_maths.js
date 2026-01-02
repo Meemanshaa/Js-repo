f81f5b6fe3ea1a9055e9719c7d11b51d5320787d
Meemansha Sumit <139813612+Meemanshaa@users.noreply.github.com>
2026-01-02 09:31:33 +0000

    numbers

---
+++ 01_basics/07_numbers_maths.js
@@ -0,0 +1,36 @@
+// const score=400
+// console.log(score)
+
+//object
+// const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(typeof(balance))
+//precision value
+// console.log(balance.toFixed(2))
+
+// const otherno=23.8966
+// console.log(otherno.toPrecision(2))
+
+const h=1000000;
+// console.log(h.toLocaleString())
+// console.log(h.toLocaleString('en-IN'))
+
+//MAX AND ,MIN
+
+
+//************************* MATH *******************************/
+// console.log(Math)
+// console.log(Math.abs(-7))
+// console.log(Math.round(6.89))
+// console.log(Math.ceil(8.78))
+// console.log(Math.floor(9.83))
+// console.log(Math.min(3,6,2,4))
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+
+const min=10;
+const max=15
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
